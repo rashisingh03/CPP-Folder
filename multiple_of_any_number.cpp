212a104e8f1bbb868056b58de713229aa1454ca7
rashisingh03 <rashi002345@gmail.com>
2024-08-05 21:06:16 +0530

    Add files via upload

---
+++ multiple_of_any_number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the number whose table you want to print:"<<endl;
+cin>>n;
+for(int i=1;i<=10;i++){
+    cout << n << " * "<< i << " = " << n* i << endl;
+}
+return 0;
+}
